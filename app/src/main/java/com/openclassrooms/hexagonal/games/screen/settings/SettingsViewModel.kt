5aecb362c7a432297290c5ce9f31e1d0f42305e8
Lucas Bigeard <luc.allau@gmail.com>
2025-01-29 02:21:35 +0100

    chore: add subscription management to firebase for background notification
    Adds string resources for subscribe/unsubscribe success/failure.
    Adds topic subscription and unsubscription logic to the settings view model.

--- app/src/main/java/com/openclassrooms/hexagonal/games/screen/settings/SettingsViewModel.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/screen/settings/SettingsViewModel.kt
@@ -1,8 +1,11 @@
 package com.openclassrooms.hexagonal.games.screen.settings
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import com.google.firebase.messaging.FirebaseMessaging
+import com.openclassrooms.hexagonal.games.R
 import com.openclassrooms.hexagonal.games.data.manager.NotificationManagerHelper
 
 /**
@@ -20,7 +23,14 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
     fun enableNotifications() {
         notificationManagerHelper.enableNotifications()
 
-        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+        FirebaseMessaging.getInstance().subscribeToTopic("All").addOnCompleteListener { task ->
+            var msg = getApplication<Application>().applicationContext.getString(R.string.subscribe_successful)
+            if (!task.isSuccessful) {
+                msg = getApplication<Application>().applicationContext.getString(R.string.subscribe_failed)
+            }
+            Log.d("SettingsViewModel", msg)
+            Toast.makeText(getApplication<Application>().applicationContext, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 
     /**
@@ -31,7 +41,14 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
 
 
         //Suppress autorisation for Firebase Messaging
-        FirebaseMessaging.getInstance().isAutoInitEnabled = false
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("All").addOnCompleteListener { task ->
+            var msg = getApplication<Application>().applicationContext.getString(R.string.unsubscribe_successful)
+            if (!task.isSuccessful) {
+                msg = getApplication<Application>().applicationContext.getString(R.string.unsubscribe_failed)
+            }
+            Log.d("SettingsViewModel", msg)
+            Toast.makeText(getApplication<Application>().applicationContext, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
